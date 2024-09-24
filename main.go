c1400cc9b426b585cb4f621a627854d9d4e47af9
gufranlazuardi <gufranlazuardi@gmail.com>
2024-09-24 11:17:33 +0700

    feat: api versioning

--- main.go
+++ main.go
@@ -1,83 +1,94 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
-func main()  {
-	router:= gin.Default()
+func main() {
+	router := gin.Default()
 
-	router.GET("/", rootHandler)
-	router.GET("/hello", helloHandlrer)
-	router.GET("/books/:id/:title", booksHandler)
-	router.GET("/query", queryHandler)
-	router.GET("/price", priceHandler)
+	v1 := router.Group("/v1")
+
+	v1.GET("/", rootHandler)
+	v1.GET("/hello", helloHandlrer)
+	v1.GET("/books/:id/:title", booksHandler)
+	v1.GET("/query", queryHandler)
+	v1.GET("/price", priceHandler)
 
 	router.POST("/books", postBooksHandler)
 
 	router.Run()
 }
 
-func rootHandler(c *gin.Context){
+func rootHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"name" : "Gufran Lazuardi",
-		"title" : "Principal Software Engineer",
+		"name":  "Gufran Lazuardi",
+		"title": "Principal Software Engineer",
 	})
 }
 
 func helloHandlrer(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"name" : "Hello",
-		"title" : "Selamat pagi",
+		"name":  "Hello",
+		"title": "Selamat pagi",
 	})
 }
 
-func booksHandler(c *gin.Context){
+func booksHandler(c *gin.Context) {
 	id := c.Param("id")
 	title := c.Param("title")
 
-	c.JSON(http.StatusOK, gin.H{"id": id , "title": title})
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
 }
 
-func queryHandler(c *gin.Context){
+func queryHandler(c *gin.Context) {
 	title := c.Query("title")
 
-	c.JSON(http.StatusOK, gin.H{"title" : title})
-
+	c.JSON(http.StatusOK, gin.H{"title": title})
 }
 
 func priceHandler(c *gin.Context) {
 	price := c.Query("price")
 	title := c.Query("title")
 
-	c.JSON(http.StatusOK, gin.H{"price" : price, "amount" : title})
-
+	c.JSON(http.StatusOK, gin.H{"price": price, "amount": title})
 	// contoh http://localhost:8080/price?price=20000&amount=10
 }
 
 type BookInput struct {
-	Title string `json:"title" binding:"required"`
-	Price int `json:"price binding:"required,number"`
-	SubTitle string `json:"sub_title"`
+	Title    string      `json:"title" binding:"required"`
+	Price    json.Number `json:"price" binding:"required,number"`
+	SubTitle string      `json:"sub_title"`
 }
 
-func postBooksHandler(c *gin.Context){
+func postBooksHandler(c *gin.Context) {
 	var bookInput BookInput
 
-	err := c.ShouldBindBodyWithJSON(&bookInput)
+	// Use ShouldBindJSON instead of ShouldBindBodyWithJSON
+	err := c.ShouldBindJSON(&bookInput)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-		fmt.Println(err)
-		return 
+		errorMessages := []string{}
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error in field %s, condition: %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMessages,
+		})
+
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"title" : bookInput.Title,
-		"price" : bookInput.Price,
-		"sub_title" : bookInput.SubTitle,
+		"title":     bookInput.Title,
+		"price":     bookInput.Price,
+		"sub_title": bookInput.SubTitle,
 	})
 }
